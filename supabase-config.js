447e08f979ebae653c994411fa9de7272b099a5b
MesuTSafa61 <mesutsafa61@gmail.com>
2026-09-13 22:55:43 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,14 @@
+/* =========================================================
+   AKÇAABAT HABER
+   SUPABASE BAĞLANTI AYARLARI
+   ========================================================= */
+
+(function () {
+    "use strict";
+
+    window.AKCAABAT_SUPABASE = {
+        url: "https://wokgvwffbootbhqxfttm.supabase.co",
+        key: "sb_publishable__ejHKVEaMQnvOU8MeAJurg_dT2tpPeD"
+    };
+
+})();
